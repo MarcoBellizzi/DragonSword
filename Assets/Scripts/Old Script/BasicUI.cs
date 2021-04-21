a51ac3b0655b941dae8c08f5f3a34e3ac1215ea4
Marco Bellizzi <marcobellizzi96@gmail.com>
2021-04-21 16:31:11 +0200

    Added castle and other stuff

---
+++ Assets/Scripts/Old Script/BasicUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicUI : MonoBehaviour
+{
+    void OnGUI()
+    {
+        if(GUI.Button(new Rect(10, 10, 40, 20), "Test"))
+        {
+            Debug.Log("Testo");
+        }
+
+        GUI.Label(new Rect(10, 20, 40, 20), "Label");
+    }
+}
---
+++ Assets/Scripts/Old Script/SceneControllerN.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneControllerN : MonoBehaviour
+{
+    [SerializeField] private GameObject enemyPrefab;
+    private GameObject[] _enemies;
+    private int enemiesCount = 7;
+
+    void Start()
+    {
+        _enemies = new GameObject[enemiesCount];
+    }
+
+    void Update()
+    {
+        for(int i=0; i<_enemies.Length; i++)
+        {
+            if(_enemies[i] == null)
+            {
+                _enemies[i] = Instantiate(enemyPrefab, new Vector3(Random.Range(1f, 5f), 1f, Random.Range(1f, 5f)), Quaternion.Euler(0, Random.Range(0, 360f), 0));
+                /*_enemies[i] = Instantiate(enemyPrefab) as GameObject;
+                _enemies[i].transform.position = new Vector3(Random.Range(1f, 5f), 1f, Random.Range(1f, 5f));
+                float angle = Random.Range(0, 360f);
+                _enemies[i].transform.Rotate(0, angle, 0);*/
+            }
+        }
+    }
+}
