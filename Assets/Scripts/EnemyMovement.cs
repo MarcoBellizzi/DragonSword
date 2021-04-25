14d6028831b8b07aebac2c297b050cdf4cc22865
Marco Bellizzi <marcobellizzi96@gmail.com>
2021-04-25 19:20:36 +0200

    Added enemy AI

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    
+    [SerializeField] private float walkSpeed;
+
+    private Vector3 moveDirection;
+
+    private CharacterController controller;
+    private Animator animator;
+
+    public NavMeshAgent agent;
+    
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move() ;
+
+    //    if (Input.GetKeyDown(KeyCode.Mouse0))
+    //    {
+    //        StartCoroutine(Attack());
+    //    }
+    }
+
+    private void Move()
+    {
+        GameObject player = GameObject.Find("Player");
+
+        if (Vector3.Distance(player.transform.position, controller.transform.position) < 2f)
+        {
+            StartCoroutine(Attack());
+        }
+        else
+        {
+            if (Vector3.Distance(player.transform.position, controller.transform.position) < 6f)
+            {
+                Walk();
+                agent.SetDestination(player.transform.position);
+            }
+            else
+            {
+                Idle();
+            }
+        }
+        
+
+    }
+
+    private void Idle()
+    {
+        animator.SetFloat("Speed", 0, 0.1f, Time.deltaTime);
+    }
+
+    private void Walk()
+    {
+        animator.SetFloat("Speed", 0.5f, 0.1f, Time.deltaTime);
+    }
+
+    private IEnumerator Attack()
+    {
+        animator.SetLayerWeight(animator.GetLayerIndex("Attack layer"), 1);
+        animator.SetTrigger("Attack");
+        
+        yield return new WaitForSeconds(0.9f);
+        animator.SetLayerWeight(animator.GetLayerIndex("Attack layer"), 0);
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,15 +10,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float runSpeed;
 
     private Vector3 moveDirection;
-    private Vector3 velocity;
 
-    [SerializeField] private bool isGrounded;
-    [SerializeField] private float groundCheckDistance;
-    [SerializeField] private LayerMask groundMask;
-    [SerializeField] private float gravity;
-
-    [SerializeField] private float jumpHeight;
-    
     private CharacterController controller;
     private Animator animator;
     void Start()
@@ -41,45 +33,29 @@ public class PlayerMovement : MonoBehaviour
 
     private void Move()
     {
-        isGrounded = Physics.CheckSphere(transform.position, groundCheckDistance, groundMask);
-
-        if (isGrounded && velocity.y < 0)
-        {
-            velocity.y = -2f;
-        }
-        
         float moveZ = Input.GetAxis("Vertical");
+        float moveX = Input.GetAxis("Horizontal");
 
-        moveDirection = new Vector3(0, 0, moveZ);
-        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection = new Vector3(moveX, 0, moveZ);
+        moveDirection = transform.TransformDirection(moveDirection);  // diventano variabili locali
 
-        if (isGrounded)
+        if (moveDirection != Vector3.zero && !Input.GetKey(KeyCode.LeftShift))
         {
-            if (moveDirection != Vector3.zero && !Input.GetKey(KeyCode.LeftShift))
-            {
-                Walk();
-            } 
-            else if (moveDirection != Vector3.zero && Input.GetKey(KeyCode.LeftShift))
-            {
-                Run();
-            } 
-            else if (moveDirection == Vector3.zero)
-            {
-                Idle(); 
-            }
-            
-            moveDirection *= moveSpeed;
-
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                Jump();
-            }
+            Walk();
+        } 
+        else if (moveDirection != Vector3.zero && Input.GetKey(KeyCode.LeftShift))
+        {
+            Run();
+        } 
+        else if (moveDirection == Vector3.zero)
+        {
+            Idle(); 
         }
-        
+            
+        moveDirection *= moveSpeed;
+
         controller.Move(moveDirection * Time.deltaTime);
 
-        velocity.y += gravity + Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
     }
 
     private void Idle()
@@ -98,12 +74,7 @@ public class PlayerMovement : MonoBehaviour
         moveSpeed = runSpeed;
         animator.SetFloat("Speed", 1, 0.1f, Time.deltaTime);
     }
-
-    private void Jump()
-    {
-        velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
-    }
-
+    
     private IEnumerator Attack()
     {
         animator.SetLayerWeight(animator.GetLayerIndex("Attack layer"), 1);
